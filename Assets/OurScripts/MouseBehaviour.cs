39325498c1b36988cf6e6b39ab72e2c099e27ac8
Mabrima <robinkarlola.arkblad@gmail.com>
2019-10-07 11:27:33 +0200

    Initial commit

---
+++ Assets/OurScripts/MouseBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBehaviour : MonoBehaviour
+{
+    public Material selectedMaterial;
+    private Material defaultMaterial;
+    private MeshRenderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+        defaultMaterial = rend.material;
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material = selectedMaterial;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material = defaultMaterial;
+    }
+
+
+}
---
+++ Assets/OurScripts/UnitBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBase : ScriptableObject
+{
+
+    public int health;
+    public int damage;
+    public int speed;
+    public int initiative;
+    public int attack;
+    public int defence;
+
+
+}
