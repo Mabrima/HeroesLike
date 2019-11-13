17c0637afc9753f7dda5710bd9b0f931549d2431
Mabrima <31730695+Mabrima@users.noreply.github.com>
2019-11-13 21:41:28 +0100

    Fixed typo in damage calculation

--- Assets/OurScripts/UnitBase.cs
+++ Assets/OurScripts/UnitBase.cs
@@ -27,12 +27,12 @@ public class UnitBase : ScriptableObject
         float damageMultiplier = 1;
         if (otherAttack > defence)
         {
-            damageMultiplier += Mathf.Clamp((otherAttack - defence) * increasePerAttack, 0, 3);
+            damageMultiplier += Mathf.Clamp((otherAttack - defence) * increasePerAttack, 0, 4);
         }
 
         if (otherAttack < defence)
         {
-            damageMultiplier -= Mathf.Clamp((defence - otherAttack) * decreasePerDefence, 0, .7f);
+            damageMultiplier -= Mathf.Clamp((defence - otherAttack) * decreasePerDefence, 0, .75f);
         }
 
         return (int) Mathf.Max(otherDamage * damageMultiplier, 1);
